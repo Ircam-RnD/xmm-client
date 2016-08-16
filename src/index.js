edc670b7a5333999d9d56448194113f2d8bcc90f
Joseph <joseph.larralde@gmail.com>
2016-08-16 15:49:34 +0200

    updated main index.js file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as GmmDecoder } from './gmm';
-export { default as HhmmDecoder } from './hmm';
+export { default as HhmmDecoder } from './hhmm';
+export { default as PhraseMaker } from './phrase';
