ef5efb965748b0aed81cdf771f62da28b391a120
Joseph <joseph.larralde@gmail.com>
2016-08-16 15:50:05 +0200

    updated main index.js file

--- dist/index.js
+++ dist/index.js
@@ -13,14 +13,23 @@ Object.defineProperty(exports, 'GmmDecoder', {
   }
 });
 
-var _hmm = require('./hmm');
+var _hhmm = require('./hhmm');
 
 Object.defineProperty(exports, 'HhmmDecoder', {
   enumerable: true,
   get: function get() {
-    return _interopRequireDefault(_hmm).default;
+    return _interopRequireDefault(_hhmm).default;
+  }
+});
+
+var _phrase = require('./phrase');
+
+Object.defineProperty(exports, 'PhraseMaker', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_phrase).default;
   }
 });
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7O3dDQUFTLE87Ozs7Ozs7Ozt3Q0FDQSxPIiwiZmlsZSI6ImluZGV4LmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IHsgZGVmYXVsdCBhcyBHbW1EZWNvZGVyIH0gZnJvbSAnLi9nbW0nO1xuZXhwb3J0IHsgZGVmYXVsdCBhcyBIaG1tRGVjb2RlciB9IGZyb20gJy4vaG1tJzsiXX0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7O3dDQUFTLE87Ozs7Ozs7Ozt5Q0FDQSxPOzs7Ozs7Ozs7MkNBQ0EsTyIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCB7IGRlZmF1bHQgYXMgR21tRGVjb2RlciB9IGZyb20gJy4vZ21tJztcbmV4cG9ydCB7IGRlZmF1bHQgYXMgSGhtbURlY29kZXIgfSBmcm9tICcuL2hobW0nO1xuZXhwb3J0IHsgZGVmYXVsdCBhcyBQaHJhc2VNYWtlciB9IGZyb20gJy4vcGhyYXNlJzsiXX0=
